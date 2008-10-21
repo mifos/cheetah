ae0fad2f31ffc194d4cd7ce2c711bad2c415a75f
Adam Feuer <adamf@pobox.com>
2008-10-21 19:15:01 +0000

    adding a file I forgot
    git-svn-id: file:///home/adamm/dist/mifos_mirror/cheetah/trunk@13988 a8845c50-7012-0410-95d3-8e1449b9b1e4

---
+++ testing/src/test/java/framework/pageobjects/DeleteLoanProductResultPage.java
@@ -0,0 +1,11 @@
+package framework.pageobjects;
+
+import com.thoughtworks.selenium.Selenium;
+
+public class DeleteLoanProductResultPage extends AbstractPage {
+
+    public DeleteLoanProductResultPage(Selenium selenium) {
+        super(selenium);
+    }
+
+}
